29e17b3c3d2566c7065b842f577baaf187bcb450
Beatriz Silva <126727432+BeatrizSRocha@users.noreply.github.com>
2025-08-19 10:40:20 -0300

    Add files via upload

---
+++ AgeComparison_exercicio/AgeComparison.cs
@@ -0,0 +1,31 @@
+﻿namespace MyApp
+{
+    internal class AgeComparison
+    {
+        static void Main(string[] args)
+        {
+            int age1, age2;
+
+            Console.WriteLine("Insira a idade do primeiro aluno: ");
+            age1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a idade do segundo aluno: ");
+            age2 = int.Parse(Console.ReadLine());
+
+            if (age1 > age2)
+            {
+                Console.WriteLine("O primeiro aluno é o mais velho.");
+                Console.WriteLine("O segundo aluno é o mais novo.");
+            }
+            if (age2 > age1)
+            {
+                Console.WriteLine("O segundo aluno é o mais velho.");
+                Console.WriteLine("O primeiro aluno é o mais novo.");
+            }
+            else
+            {
+                Console.WriteLine("Ambos os alunos têm a mesma idade.");
+            }
+        }
+    }
+}
