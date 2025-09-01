4df14197171ae18b537607845804b7f4712e2b48
Beatriz Silva <126727432+BeatrizSRocha@users.noreply.github.com>
2025-09-01 11:37:10 -0300

    aula

---
+++ Aula.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    int i = 0;
+        //    while (i < 10000) {
+        //        Console.WriteLine("Alguma coisa que vai imprimir");
+        //        i++;
+        //    }
+        //}
+        static void Main(string[] args)
+        { 
+            // Inicialização da variável, condição de funcionamento, incremento
+            for (int i = 0; i <= 9; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Loopingsforewhile_exercicio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Loopingsforewhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 'a' define a altura da pirâmide (número de linhas)
+            int a = 10;
+
+            for (int i = 0; i < a; i++)
+            {
+
+                // O número de espaços diminui à medida que o 'i' aumenta
+                for (int j = 0; j < a - i - 1; j++)
+                {
+                    Console.Write(" "); // Imprime um espaço
+                }
+
+                // Com o os asteriscos no centro, o número deles aumenta conforme o 'i' cresce
+                for (int k = 0; k < (2 * i + 1); k++)
+                {
+                    Console.Write("*");
+                }
+
+                // Após inserir todos os asteriscos, pula uma linha, formando o formato do triângulo
+                Console.WriteLine();
+            }
+        }
+    }
+}
