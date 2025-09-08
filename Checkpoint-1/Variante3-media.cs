e1e3d1d26ad7c48d8832d96ae1eb483d1dd5a9f8
Beatriz Silva <126727432+BeatrizSRocha@users.noreply.github.com>
2025-09-08 10:40:03 -0300

    Create Variante3-media.cs

---
+++ Checkpoint-1/Variante3-media.cs
@@ -0,0 +1,34 @@
+﻿// Projeto de Avaliação (Revisado) - Variante 3: Cálculo de média de três notas
+// Este programa deve solicitar o nome do estudante e três notas, calcular e exibir a média.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o nome do estudante:");
+
+        string nome = Console.ReadLine();
+        double soma = 0;
+
+        for (int i = 0; i <= 2; i++)
+        {
+            Console.WriteLine("Digite a nota:");
+            string nota_calculo = Console.ReadLine();
+
+            if (double.TryParse(nota_calculo, out double nota))
+            {
+                soma += nota;
+            }
+            else
+            {
+                Console.WriteLine("Nota inválida. Tente novamente.");
+            }
+        }
+
+        double media = soma / 3;
+        Console.WriteLine("A média do estudante " + nome + " é " + media);
+    }
+}
+
